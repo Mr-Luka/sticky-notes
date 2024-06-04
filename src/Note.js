5ce57915c8a80205a67f7278ee03fddb5ac456ea
Luka <luka.luka.luka93@gmail.com>
2024-06-04 14:52:58 -0700

    decomposing Note into its own module

---
+++ src/Note.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Note extends Component {
+    render() {
+        return (
+            <li className="note">
+                <input className="note__title" type="text" placeholder="Title"/>
+                <textarea className="note__description" placeholder="Description..."/>
+                <span className="note__delete">X</span>
+            </li>
+        )
+    }
+}
+
+export default Note;
