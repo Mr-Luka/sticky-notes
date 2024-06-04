cc9ae3a23e29e96fd9be48e4d66523f7948dcee2
Luka <luka.luka.luka93@gmail.com>
2024-06-04 14:27:05 -0700

    changed the way App renders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App/>);
