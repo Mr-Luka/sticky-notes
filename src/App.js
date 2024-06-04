af0af6e5198914ea750117223d32a1bac0cf08d3
Luka <luka.luka.luka93@gmail.com>
2024-06-04 14:21:07 -0700

    index.css render App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+    <div>
+        <header>
+            <h1>Super Sticky Notes</h1>
+            <aside>
+                <button>+ New Note</button>
+                <input type="text" placeholder="Type here to seach..."/>
+            </aside>
+        </header>
+    </div>
+}
+
+export default App;
