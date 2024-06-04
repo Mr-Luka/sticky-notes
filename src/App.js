c816a3ebedb5a9e06278fd35b735c0e4e0e3c1bb
Luka <luka.luka.luka93@gmail.com>
2024-06-04 14:56:09 -0700

    added Header and NotesList into the App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "./Header.js";
+import NotesList from "./NotesList.js";
+
 import "./index.css";
 
 const App = () => (
     <div>
-
+        <Header/>
+        <NotesList/>
     </div>
 );
 
