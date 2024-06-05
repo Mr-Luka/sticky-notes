2aa4e8fb82f049a57cdcf3178f8d336a9a4d915c
Luka <luka.luka.luka93@gmail.com>
2024-06-05 14:10:58 -0700

    mapping the notes and renderNotes

--- src/NotesList.js
+++ src/NotesList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Note from "./Note.js";
 
-const NotesList =()=> (
-    <ul className="notes-list">
-        <Note/>
-        <Note/>
-        <Note/>
-    </ul>
-)
+const NotesList = props => {
+    const renderNotes = note => <Note note={note} key={note.id}/>
+    const noteElements = props.notes.map(renderNotes);
+    return (
+    <ul className="notes-list">{noteElements}</ul>
+    )
+}
 
 
 export default NotesList;
