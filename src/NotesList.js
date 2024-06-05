355d3c0bdfd978c55ede1be984362894d503c3d4
Luka <luka.luka.luka93@gmail.com>
2024-06-05 14:20:43 -0700

    using the filter method to remove any objects from the notes array that have a doesMatchSearch value of false

--- src/NotesList.js
+++ src/NotesList.js
@@ -2,8 +2,10 @@ import React from "react";
 import Note from "./Note.js";
 
 const NotesList = props => {
+    const keepSearchMatch = note => note.doesMatchSearch
+    const searchMatches = props.notes.filter(keepSearchMatch)
     const renderNotes = note => <Note note={note} key={note.id}/>
-    const noteElements = props.notes.map(renderNotes);
+    const noteElements = searchMatches.map(renderNotes);
     return (
     <ul className="notes-list">{noteElements}</ul>
     )
