2b7f06178aaa5dc1fcae18b0263fea66b485100c
Luka <luka.luka.luka93@gmail.com>
2024-06-05 14:14:01 -0700

    setting the value attribute of the text input element in Header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-const Header = ()=> (
+const Header = props=> (
             <header className="app-header">
             <h1 className="app-header__title">Super Sticky Notes</h1>
             <aside className="app-header__controls">
                 <button className="add-new">+ New Note</button>
-                <input className="search" type="text" placeholder="Type here to seach..."/>
+                <input 
+                    value={props.searchText}
+                    className="search" 
+                    type="text" 
+                    placeholder="Type here to seach..."/>
             </aside>
         </header>
 )
