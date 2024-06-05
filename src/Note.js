b1764b636a3381fab9c2e011190a82467737f192
Luka <luka.luka.luka93@gmail.com>
2024-06-05 14:18:03 -0700

    setting the title and description value attributes in Note.js

--- src/Note.js
+++ src/Note.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Note =()=> (
+const Note = props => (
     <li className="note">
-        <input className="note__title" type="text" placeholder="Title"/>
-        <textarea className="note__description" placeholder="Description..."/>
+        <input 
+            value={props.note.title}
+            className="note__title" 
+            type="text" 
+            placeholder="Title"/>
+        <textarea 
+            value={props.note.description}
+            className="note__description" 
+            placeholder="Description..."/>
         <span className="note__delete">X</span>
     </li>
 )
