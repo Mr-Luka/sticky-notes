b375d6f4f79e500a9ebf5775bea446cc3bb41c36
Luka <luka.luka.luka93@gmail.com>
2024-06-05 14:28:27 -0700

    filter matches and put them in note elements for the mapping

--- src/NotesList.js
+++ src/NotesList.js
@@ -2,8 +2,10 @@ import React from "react";
 import Note from "./Note.js";
 
 const NotesList = props => {
+    const keepNoteMatch = note => note.doesMatchSearch;
+    const filterMatch = props.notes.filter(keepNoteMatch);
     const renderNote = note => <Note note={note} key={note.id}/>
-    const noteElements = props.notes.map(renderNote);
+    const noteElements = filterMatch.map(renderNote);
     return (
     <ul className="notes-list">{noteElements}</ul>
     )
