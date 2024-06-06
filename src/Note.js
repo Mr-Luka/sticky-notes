9df93b904a6f1769d1678acb7ce7e17b8d292666
Luka <luka.luka.luka93@gmail.com>
2024-06-06 13:13:41 -0700

    updateTitle in the notes

--- src/Note.js
+++ src/Note.js
@@ -1,19 +1,31 @@
 import React from "react";
 
-const Note = props => (
+const Note = props => {
+    const updateTitle = e => {
+        const updateValue = e.target.value;
+        const editMeId = props.note.id;
+        props.onType(editMeId, "title", updateValue)
+    }
+
+
+    return (
     <li className="note">
         <input 
             value={props.note.title}
             className="note__title" 
             type="text" 
-            placeholder="Title"/>
+            placeholder="Title"
+            onChange={updateTitle}
+            />
         <textarea 
             value={props.note.description}
             className="note__description" 
-            placeholder="Description..."/>
+            placeholder="Description..."
+            onChange = {props.onType}
+            />
         <span className="note__delete">X</span>
     </li>
 )
-
+}
 
 export default Note;
