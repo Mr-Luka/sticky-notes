b2752d995460ecc547e2fe89fc8f77bbe6fcc47b
Luka <luka.luka.luka93@gmail.com>
2024-06-06 13:15:45 -0700

    adding onType in the notes list

--- src/NotesList.js
+++ src/NotesList.js
@@ -4,7 +4,7 @@ import Note from "./Note.js";
 const NotesList = props => {
     const keepNoteMatch = note => note.doesMatchSearch;
     const filterMatch = props.notes.filter(keepNoteMatch);
-    const renderNote = note => <Note note={note} key={note.id}/>
+    const renderNote = note => <Note note={note} key={note.id} onType={props.onType}/>
     const noteElements = filterMatch.map(renderNote);
     return (
     <ul className="notes-list">{noteElements}</ul>
