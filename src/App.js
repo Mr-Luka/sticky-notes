4fa9691e4235f17ab7d5f60ae71e706e6bc1ff46
Luka <luka.luka.luka93@gmail.com>
2024-06-11 14:16:47 -0700

    setup the project, non reactive notes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, {Component} from "react";
-import Header from "./Header.js";
-import NotesList from "./NotesList.js";
+
 
 import "./index.css";
 
 class App extends Component {
 
+    render (){
 
+        return (
+          <div>
+            <header>
+                <h1>Sticky Notes</h1>
+                <aside>
+                    <button>+ New Note</button>
+                    <input type="text" placeholder="Type here to search"/>
+                </aside>
+            </header>
+            <ul>
+                <li>
+                    <input type="text" placeholder="Title"/>
+                    <textarea placeholder="Description..."/>
+                    <span>X</span>
+                </li>
+            </ul>
+          </div>  
+        );
+    }
+}
 
-};
 
 export default App;
