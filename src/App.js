5cffeed1783747ecb85e7b2599cd644556cf77fa
Luka <luka.luka.luka93@gmail.com>
2024-06-11 14:24:47 -0700

    separated the html from App component into different modules/components, Header, Note and NotesList.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import Header from "./Header.js"
+import NotesList from "./NotesList.js";
 
 
 import "./index.css";
@@ -9,20 +11,8 @@ class App extends Component {
 
         return (
           <div>
-            <header>
-                <h1 className="app-header__title">Sticky Notes</h1>
-                <aside className="app-header__controls">
-                    <button className="add-new">+ New Note</button>
-                    <input className="search" type="text" placeholder="Type here to search"/>
-                </aside>
-            </header>
-            <ul className="notes-list">
-                <li className="note">
-                    <input className="note__title" type="text" placeholder="Title"/>
-                    <textarea className="note__description" placeholder="Description..."/>
-                    <span className="note__delete">X</span>
-                </li>
-            </ul>
+            <Header/>
+            <NotesList/>
           </div>  
         );
     }
