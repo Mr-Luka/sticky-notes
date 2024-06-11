2569c3554374589248c1ffe8d36600a4d5459b88
Luka <luka.luka.luka93@gmail.com>
2024-06-11 14:51:33 -0700

    made a new method to addNote

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 
-const Header = () => (
+const Header = (props) => (
     <header>
         <h1 className="app-header__title">Sticky Notes</h1>
         <aside className="app-header__controls">
-            <button className="add-new">+ New Note</button>
-            <input className="search" type="text" placeholder="Type here to search"/>
+            <button 
+                className="add-new"
+                onClick={props.addNote}
+                >+ New Note</button>
+            <input 
+                className="search" 
+                type="text" 
+                placeholder="Type here to search"
+                value={props.state.searchText}
+                />
         </aside>
     </header>
 )
