a66c154cddcbbc10292fe69dad6402f0e706d59c
Luka <luka.luka.luka93@gmail.com>
2024-06-11 14:06:19 -0700

    starting over

--- src/App.js
+++ src/App.js
@@ -5,53 +5,9 @@ import NotesList from "./NotesList.js";
 import "./index.css";
 
 class App extends Component {
-    state = {
-        notes: [
-        {
-            id: Date.now(),
-            title: "",
-            description: "",
-            doesMatchSearch: true,
-        }
-     ],
-     searchText: "",
-    }
-    addNote = () => {
-        const newNote = {
-            id: Date.now(),
-            title: "",
-            description: "",
-            doesMatchSearch: true,
-            }
-        const newNotes = [newNote, ...this.state.notes];
-        this.setState({notes: newNotes});
-    }
 
-    onType = (editMeId, updateKey, updatedValue) => {
-        const updateNotes = this.state.notes.map((note)=>{
-            if (note.id !== editMeId) {
-                return note;
-            } else {
-                if(updateKey === "title") {
-                    note.title = updatedValue;
-                    return note;
-                } else {
-                    note.description = updatedValue;
-                    return note;
-                }
-            }
-        });
-        this.setState({notes: updateNotes})
-    }
 
-    render () {
-        return(
-        <div>
-            <Header searchText={this.state.searchText} addNote={this.addNote}/>
-            <NotesList notes={this.state.notes} onType = {this.onType}/>
-        </div>
-        )
-    }
+
 };
 
 export default App;
