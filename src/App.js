d761fe4b68439bcb8300594901de2b1a6857842a
Luka <luka.luka.luka93@gmail.com>
2024-06-11 14:27:46 -0700

    assigning data to components, setting the state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ import NotesList from "./NotesList.js";
 import "./index.css";
 
 class App extends Component {
+    state= {
+        notes: [{
+        id: 0,
+        title: "eat",
+        description: "reese peanut butter cups",
+        doesMatchSearch: true
+      },
+      {
+        id: 1,
+        title: "sleep",
+        description: "eight hours",
+        doesMatchSearch: true
+      },
+      {
+        id: 2,
+        title: "code",
+        description: "build an awesome ui",
+        doesMatchSearch: true
+      }],
+      searchText: "",
+    }
 
     render (){
 
